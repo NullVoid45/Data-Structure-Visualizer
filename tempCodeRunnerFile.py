9b362ce24981d3cdb91be9da114548c31fc19238
Devesh <151498881+NullVoid45@users.noreply.github.com>
2025-08-28 07:09:18 +0530

    Delete tempCodeRunnerFile.py

--- tempCodeRunnerFile.py
+++
@@ -1,6 +0,0 @@
-if photo_img:
-    web_button = tk.Button(scr, image=photo_img, command=Web, bg="#505a5b", width=40, height=20)
-    web_button.place(x=140, y=5)
-else:
-    web_button = tk.Button(scr, text="Web", command=Web, bg="#505a5b", fg="#000000", width=5, height=1)
-    web_button.place(x=140, y=5)
